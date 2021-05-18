a84ba8cf1ee465390e0ba6ad5402a9a901535b53
techyminati <sinha.aryan03@gmail.com>
2021-05-18 10:03:19 +0000

    tiare: Fixup!
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- omni_tiare.mk
+++ omni_tiare.mk
@@ -14,10 +14,7 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-$(call inherit-product, device/xiaomi/tiarefull_tiare.mk)
+$(call inherit-product, device/xiaomi/tiare/full_tiare.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
